51799491d4ecc83ec4ef97ff25633a92af1fc05d
RafaQuant <rafa.bordafx@gmail.com>
2024-06-27 01:23:52 -0300

    Percentage variation was addded      merval_historical_monthly_values.loc[         index2.year,         months[index2.month - 1]         ] = fila['Adj Close']
         merval_historical_monthly_values.loc[
            index2.year,
            months[index2.month - 1]
            ] = fila['Adj Close']

--- monthly_historical_state.py
+++ monthly_historical_state.py
@@ -10,62 +10,56 @@ merval_ars = "M.BA"
 
 #Cómo el periodo que especifíco es "max", no hace falta que ponga start y end.
 merval = yf.download(merval_ars, interval="1wk", period="max")
-merval = merval['Adj Close']
+
 
 #Hago el resample porque vía yf.download() con interval="1mo" no me traía 
 #   bien los datos.
 merval_resample = merval.copy().resample('M').last()
 
 
+
 datetime_years_index = []
 months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo",
     "Junio", "Julio", "Agosto", "Septiembre", "Octubre",
     "Noviembre", "Diciembre"]
-months_lists = {}
+
 
 for index in merval_resample.index:
     if index.year not in datetime_years_index:
         datetime_years_index.append(index.year)
-    merval_historical_monthly_state = pd.DataFrame(
-        index=datetime_years_index,
-        columns=months
+    merval_historical_monthly_values = pd.DataFrame(
+        index=datetime_years_index, #Seteo índice eje Y
+        columns=months #Seteo índice eje X
         )
-    months_lists[
-        months[index.month - 1]
-        ] = merval_resample[index]
+    merval_historical_monthly_pct = pd.DataFrame(
+        index=datetime_years_index, 
+        columns=months 
+        )  
     
-    for key, value in enumerate(months_lists):
-        print("Key: {} - {}".format(value, months_lists[value]))
-        
+# name = index2 para que no crashee con el de arriba    
+for index2, fila in merval_resample.iterrows():
+     merval_historical_monthly_values.loc[
+        index2.year,
+        months[index2.month - 1]
+        ] = fila['Adj Close']
+     
+     
 
+merval_pct = pd.DataFrame()
 
+merval_pct['var price'] = merval_resample['Adj Close'].diff()
+merval_pct['var pct-%'] = round(
+    (
+     merval_resample['Adj Close'] / merval_resample['Adj Close'].shift(1) - 1
+     ) * 100,
+    2)
 
+for index3, fila_pct in merval_pct.iterrows():
+     merval_historical_monthly_pct.loc[
+        index3.year,
+        months[index3.month - 1]
+        ] = fila_pct['var pct-%']
+     
+     
 
-
-
-
-
-
-
-
-
-#for index in merval_resample.index:
-    # if index.year not in datetime_years_index:
-    #     datetime_years_index.append(index.year)
-    # merval_historical_monthly_state = pd.DataFrame(
-    #     index=datetime_years_index,
-    #     columns=months
-    #     )
-    # months_lists[
-    #     months[index.month - 1]
-    #     ] = merval_resample[index]
-    
-    # for key, value in enumerate(months_lists):
-    #     print("Key: {} - {}".format(value, months_lists[value]))
-        
-
-
-
-
-
-        
+     
